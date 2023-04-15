565f64e3074f58c4c10146ec940e7e69b097446d
Freddy Carrillo Tucto <70508243+FreddyCarrillo@users.noreply.github.com>
2023-04-15 07:30:14 -0500

    hasrouter added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, HashRouter} from "react-router-dom";
 import ProtectedRoute from "./components/guard/ProtectedRoute";
 import Routes from "./navigation/Route";
 import Login from './pages/auth/LoginPage';
@@ -9,21 +9,23 @@ function App() {
   return (
     <>
       <Router>
-        <Switch>
-          <Route exact path="/gift-card-customer/:id" component={GiftCardCustomer} />      
-          <Route exact path="/login" component={Login} />
-          {Routes.map((layout, i) => {
-            return (
-              <ProtectedRoute
-                key={i}
-                exact={layout.exact}
-                path={layout.path}
-                component={layout.component}
-                name={layout.name}
-              />
-            );
-          })}
-        </Switch>
+        <HashRouter>
+          <Switch>
+            <Route exact path="/gift-card-customer/:id" component={GiftCardCustomer} />      
+            <Route exact path="/login" component={Login} />
+            {Routes.map((layout, i) => {
+              return (
+                <ProtectedRoute
+                  key={i}
+                  exact={layout.exact}
+                  path={layout.path}
+                  component={layout.component}
+                  name={layout.name}
+                />
+              );
+            })}
+          </Switch>
+        </HashRouter>
       </Router>
     </>
   );
